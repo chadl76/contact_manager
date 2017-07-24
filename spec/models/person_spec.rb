1e6d01d3b02023b4e8037df4f5ae2fbb2119c97f
Chad Lucas <chad.lucas.19@gmail.com>
2017-07-24 16:38:01 -0500

    confirm person is valid in person spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-	let(:person) do
+    let(:person) do
 		Person.new(first_name: 'Al', last_name: 'Smith')
 	end
+
+  it 'is valid' do 
+  	expect(person).to be_valid
+  end
+  
   it "is invalid without a first name" do
   	person.first_name = nil
   	expect(person).to_not be_valid
